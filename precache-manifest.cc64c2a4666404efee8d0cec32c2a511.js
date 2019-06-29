6abd174646c9ac8eca3c9975753ed91baf44cc7f
Dane David <dane@surveysparrow.com>
2019-06-29 21:36:32 +0530

    Updates

---
+++ precache-manifest.cc64c2a4666404efee8d0cec32c2a511.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c399df650c40e2669ff803b8947356a",
+    "url": "/irda-questions/index.html"
+  },
+  {
+    "revision": "d97bcd5861e67011572c",
+    "url": "/irda-questions/static/css/main.4ebc21a8.chunk.css"
+  },
+  {
+    "revision": "30b910e561009ec08901",
+    "url": "/irda-questions/static/js/2.9d955685.chunk.js"
+  },
+  {
+    "revision": "d97bcd5861e67011572c",
+    "url": "/irda-questions/static/js/main.fdbdc00f.chunk.js"
+  },
+  {
+    "revision": "9c4ca8c75babb2a7cb33",
+    "url": "/irda-questions/static/js/runtime~main.5f586ed9.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/irda-questions/precache-manifest.cc64c2a4666404efee8d0cec32c2a511.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/irda-questions/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
